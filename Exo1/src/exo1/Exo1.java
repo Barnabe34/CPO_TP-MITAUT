b245a8f443ce073f555037545fdfc85d61f79cb5
Barnabé Mitaut <barnabe.mitaut@epfedu.fr>
2024-09-23 16:26:45 +0200

    Création d'un nouveau projet versionné

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,17 @@
+/*MITAUT
+TDA 
+MRG
+Septembre 2024
+*/
+package exo1;
+
+public class Exo1 {
+
+  
+    public static void main(String[] args) {
+       
+System.out.println("Bonjour");
+    }
+    
+}
+
