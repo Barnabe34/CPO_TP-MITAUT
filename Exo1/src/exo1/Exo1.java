0c75c9080f0b89767c5c120f5e41e8307110d0c0
Barnabé Mitaut <barnabe.mitaut@epfedu.fr>
2024-09-23 16:41:39 +0200

    Création d'un nouveau projet versionné

--- Exo1/src/exo1/Exo1.java
+++ Exo1/src/exo1/Exo1.java
@@ -5,12 +5,20 @@ Septembre 2024
 */
 package exo1;
 
+import java.util.Scanner;
+
 public class Exo1 {
 
   
     public static void main(String[] args) {
-       
-System.out.println("Bonjour");
+
+ String prenom;
+Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, quel est votre prenom ?");
+prenom = sc.nextLine();
+
+System.out.println("au revoir " +prenom);
     }
     
 }
