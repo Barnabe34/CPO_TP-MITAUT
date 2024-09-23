7b308a2fcd517b4f875156672e185bf2fa32eadd
Barnabé Mitaut <barnabe.mitaut@epfedu.fr>
2024-09-23 16:43:55 +0200

    Création d'un nouveau projet versionné

---
+++ Exo2/src/exo2/Exo2.java
@@ -0,0 +1,35 @@
+/*MITAUT
+TDA
+Addition d'entier
+29/03/2024
+*/
+package exo2;
+
+/**
+ *
+ * @author Barnabé
+ */
+public class Exo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       public static void main(String[] args) {
+//Declaration des variables
+int nb; // nombre dentiers a additionner
+int result; // resultat
+int ind; //indice
+nb=5;
+result=0;
+// Addition des nb premiers entiers
+ind=1;
+while (ind <= nb) {
+result=result+ind;
+}
+// Affichage du resultat
+System.out.println();
+System.out.println("La somme des "+ nb + "entiers est: "+result);
+    }
+    
+}
