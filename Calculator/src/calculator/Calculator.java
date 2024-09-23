a110feca7ff3e46f78b337a5dc300c61126773cd
Barnabé Mitaut <barnabe.mitaut@epfedu.fr>
2024-09-23 16:56:32 +0200

    calculette exo2

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,30 @@
+/*MITAUT
+TDA
+Addition d'entier
+29/03/2024
+*/
+package calculator;
+
+/**
+ *
+ * @author Barnabé
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Please enter the operator");
+        System.out.println("1) add");
+        System.out.println("2) substract");
+        System.out.println("3) multiply");
+        System.out.println("4) divide");
+        System.out.println("5) modulo");
+        
+    }  
+    
+    
+    
+    
+}
